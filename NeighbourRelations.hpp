7efed96c4868c636382eb969a027553abc8bea3b
Artyom <art.ustinov@mail.ru>
2020-11-22 22:34:01 +0300

    add neighbours

---
+++ NeighbourRelations.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Артем Устинов
+//
+
+#ifndef MAT_LOG_NEIGHBOURRELATIONS_HPP
+#define MAT_LOG_NEIGHBOURRELATIONS_HPP
+enum NeighbourRelations {
+  Right,
+  DOWN
+};
+#endif //MAT_LOG_NEIGHBOURRELATIONS_HPP
